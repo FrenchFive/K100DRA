a054ef72a9f7c4de83941465b3cd73c25f296bcc
FIVE <cahhong@artfx.fr>
2025-06-03 23:57:46 +0100

    Adding back Timedelta

--- k_youtube.py
+++ k_youtube.py
@@ -1,7 +1,7 @@
 import os
 import json
 import threading
-from datetime import datetime, timezone
+from datetime import datetime, timedelta, timezone
 import google.auth.transport.requests
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
